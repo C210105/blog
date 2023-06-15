0204470aafd2c0671cb979d4aba9ce4267943d1b
DANG LONG <long.bmw10@gmail.com>
2023-06-15 17:01:07 +0900

    login_htmlを編集した

--- src/test/java/net/shop2k/blog/BlogApplicationTests.java
+++ src/test/java/net/shop2k/blog/BlogApplicationTests.java
@@ -1,20 +1,7 @@
 package net.shop2k.blog;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import net.shop2k.blog.entitys.Articles;
-import net.shop2k.blog.entitys.Categorys;
-import net.shop2k.blog.repositorys.ArticlesRepository;
-import net.shop2k.blog.services.ArticlesService;
-import net.shop2k.blog.services.CategorysService;
 
 @SpringBootTest
 class BlogApplicationTests {
